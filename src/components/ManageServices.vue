c1bdf287e92cb528faeec892278d72b31ee91a33
Nathaniel Anum Adjah <nathanielanum13@gmail.com>
2021-06-25 01:02:27 +0100

    UI changes

--- src/components/ManageServices.vue
+++ src/components/ManageServices.vue
@@ -19,13 +19,19 @@
       <div class="services-header-options">
         <div class="data-per-page">
           <span>Number per page</span>
-          <input max="50" min="1" type="number" value="10">
+          <input v-model="numberPerPage" max="50" min="1" type="number">
         </div>
         <div class="fetch-data">
-          <button type="button" @click="fetchServices">Fetch Services</button>
+          <button type="button" class="wave" @click="fetchServices">Fetch Services</button>
         </div>
         <div class="refresh-data">
-          <button type="button" @click="fetchServices">Refresh</button>
+          <button type="button" class="wave" @click="fetchServices">Refresh</button>
+        </div>
+        <div class="add-data">
+          <button type="button" class="wave">+ Create New Service</button>
+        </div>
+        <div class="show">
+          <span>Showing 1 to 4 of 5 entries</span>
         </div>
       </div>
       <template v-if="allServices.length === 0">
@@ -37,6 +43,7 @@
             <span class="circle"></span>
             <span></span>
             <span></span>
+            <span></span>
           </div>
         </div>
       </template>
@@ -44,26 +51,35 @@
         <div class="data-section">
           <div class="data-header">
             <div class="service-icon"></div>
-            <div class="service-name" data-tooltip-text="Service Name">Service Name</div>
-            <div class="time" data-tooltip-text="Time">Time</div>
-            <div class="day" data-tooltip-text="Day">Day</div>
-            <div class="duration" data-tooltip-text="Duration In Minutes">Duration</div>
-            <div class="number-of-seats" data-tooltip-text="Number Of Seats">Seats</div>
+            <div class="service-name"><span data-tooltip-text="Service Name">Service Name</span></div>
+            <div class="time"><span data-tooltip-text="Time">Time</span></div>
+            <div class="day"><span data-tooltip-text="Day">Day</span></div>
+            <div class="duration"><span data-tooltip-text="Duration In Minutes">Duration</span></div>
+            <div class="number-of-seats"><span data-tooltip-text="Number Of Seats">Seats</span></div>
             <div class="actions"></div>
           </div>
-          <div class="data" v-for="service of allServices" :key="service.id">
+          <div v-for="service of allServices" :key="service.id" class="data">
             <div class="service-icon">
               <div :class="['icon', getColor(service.name)]">{{ getIcon(service.name) }}</div>
             </div>
-            <div class="service-name">{{service.name}}</div>
-            <div class="time">{{service.time}}</div>
-            <div class="day">{{service.day}}</div>
-            <div class="duration">{{service.durationInMinutes}}</div>
-            <div class="number-of-seats">{{service.numberOfSeats}}</div>
-            <div class="actions"></div>
+            <div class="service-name">{{ service.name }}</div>
+            <div class="time">{{ service.time }}</div>
+            <div class="day">{{ service.day }}</div>
+            <div class="duration">{{ service.durationInMinutes }}</div>
+            <div class="number-of-seats">{{ service.numberOfSeats }}</div>
+            <div class="actions">
+              <span class="wave"><i class="ri-more-2-fill"></i></span>
+              <span class="wave"><i class="ri-pencil-fill"></i></span>
+              <span class="wave"><i class="ri-delete-bin-3-fill"></i></span>
+            </div>
           </div>
         </div>
       </template>
+    </section>
+    <section class="pagination">
+      <div class="pages">
+        <span v-for="page of pages()" :key="page" class="page">{{ page }}</span>
+      </div>
     </section>
   </div>
 </template>
@@ -78,27 +94,43 @@ export default {
     const {fetchAllServices} = useService()
     const loading = ref(false)
     const allServices = ref([])
-    const loadingData = ref([1, 2, 3, 4])
+    const loadingData = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     const fetchServices = async () => {
       allServices.value = []
       loading.value = true
       await fetchAllServices().then((res) => allServices.value = res).then(() => loading.value = false)
     }
+    // Pagination
+    const currentPage = ref(1)
+    const numberPerPage = ref(10)
+    const maxPage = ref(null)
+
+    const pages = () => {
+      let allPages = [];
+      maxPage.value = Math.ceil(allServices.value.length / numberPerPage.value)
+      for (let i = 1; i <= maxPage.value; i++) {
+        allPages.push(i)
+      }
+      return allPages
+    }
+
     const getIcon = (str) => {
       return str.substring(0, 1)
     }
     const getColor = (str) => {
-      if(str.length < 10) return 'green'
-      if(str.length < 15 && str.length > 10) return 'danger'
+      if (str.length < 10) return 'green'
+      if (str.length < 15 && str.length > 10) return 'danger'
       return 'violet'
     }
     return {
       allServices,
       loading,
       loadingData,
+      numberPerPage,
       fetchServices,
       getIcon,
-      getColor
+      getColor,
+      pages
     }
   }
 }
@@ -170,7 +202,7 @@ export default {
 
   .services-table {
     width: 100%;
-    max-height: 65vh;
+    height: 65vh;
     overflow: hidden scroll;
 
     border-radius: .5rem;
@@ -197,11 +229,17 @@ export default {
       border-radius: .25rem;
       align-items: center;
 
-      .data-per-page {
+      .show {
+        flex: 1 0 auto;
+        text-align: right;
+      }
+
+      .data-per-page, .show {
         span {
           padding: .25rem .75rem;
           font-weight: bold;
           color: #656565;
+          font-size: 1.1rem;
         }
 
         input[type="number"] {
@@ -222,7 +260,7 @@ export default {
         }
       }
 
-      .refresh-data {
+      .refresh-data, .add-data {
         button {
           @include base-button;
           margin: 0 0 0 .5rem;
@@ -250,19 +288,26 @@ export default {
         background-color: color(primary-lighter);
         border-radius: .25rem;
 
-        div {
+
+        div, div > span {
           font-size: 1.1rem;
           font-weight: bold;
           color: #656565;
         }
 
-        div.service-icon {
+        .duration, .number-of-seats, .time {
           flex: 0 0 5rem;
         }
-        div:not(.service-icon) {
-          flex: 0 0 calc((100% - 5rem) / 6);
+
+        .service-icon {
+          flex: 0 0 2.5rem;
+        }
+
+        .service-name, .day, .time, .actions {
+          flex: 0 0 calc((100% - 2.5rem - 15rem) / 4);
         }
       }
+
       .data {
         width: 100%;
         border-radius: .5rem;
@@ -281,15 +326,19 @@ export default {
         padding: .75rem 1rem;
 
         div {
-          font-size: 1.05rem;
+          font-size: 1.08rem;
           font-weight: bold;
           color: #656565;
 
           @include cut-text;
         }
 
-        div.service-icon {
+        .duration, .number-of-seats, .time {
           flex: 0 0 5rem;
+        }
+
+        .service-icon {
+          flex: 0 0 2.5rem;
 
           .icon {
             @include circle(2rem);
@@ -297,9 +346,59 @@ export default {
           }
         }
 
-        div:not(.service-icon) {
-          flex: 0 0 calc((100% - 5rem) / 6);
+        .service-name, .day, .time, .actions {
+          flex: 0 0 calc((100% - 2.5rem - 15rem) / 4);
         }
+
+        .actions {
+          @include row;
+          justify-content: space-between;
+          padding: .5rem;
+          align-items: center;
+
+          span {
+            @include circle(2rem);
+
+            &:hover {
+              cursor: pointer;
+            }
+          }
+
+          span:nth-child(2) {
+            background-color: color(primary);
+            color: color(lighter);
+          }
+
+          span:nth-child(3) {
+            background-color: color(danger);
+            color: color(lighter);
+          }
+        }
+      }
+    }
+  }
+  .pagination {
+    width: max-content;
+    height: auto;
+    padding: 1rem 2rem;
+    position: fixed;
+    bottom: 5%;
+    right: 50%;
+    transform: translateX(50%);
+
+    background-color: #fff;
+
+    border-radius: .5rem;
+
+    .pages {
+      .page {
+        padding: .5rem;
+        background-color: #80808015;
+        border-radius: .25rem;
+        color: color(primary);
+        margin: 0 .25rem;
+        font-weight: bold;
+
       }
     }
   }
@@ -324,13 +423,15 @@ export default {
     @include circle(3rem);
     background-color: #80808020;
   }
+
   span {
-    width: calc(100% / 3);
+    width: calc(100% / 4);
     height: 50%;
     display: block;
     background-color: #80808015;
     border-radius: .25rem;
   }
+
   animation: slide-fade 500ms forwards linear infinite alternate-reverse;
   @keyframes slide-fade {
     to {
--- src/models/service.js
+++ src/models/service.js
@@ -1,3 +1,12 @@
-function Service () {
-
+class Service {
+    constructor(id, name, time, day, durationInMinutes, numberOfSeats, createdOn, updatedOn) {
+        this.id = id
+        this.name = name
+        this.time = time
+        this.day = day
+        this.durationInMinutes = durationInMinutes
+        this.numberOfSeats = numberOfSeats
+        this.createdOn = createdOn
+        this.updatedOn = updatedOn
+    }
 }
